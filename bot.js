590eac24a05b641cd7fb214adb459266773165e9
nitrokake <46089576+nitrokake@users.noreply.github.com>
2018-12-23 18:26:24 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+cosnt Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+});
+
+ client.on('message', message => {
+    if (message.content === 'ping') {
+      message.reply('pong');
+    }
+});
+
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
