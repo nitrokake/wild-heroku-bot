325ec6f4f237cd3757a33ec4093606b74456aa80
nitrokake <46089576+nitrokake@users.noreply.github.com>
2018-12-23 18:34:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
-cosnt Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+    console.log('I am ready!');
 });
 
- client.on('message', message => {
+client.on('message', message => {
     if (message.content === 'ping') {
-      message.reply('pong');
-    }
+    	message.reply('pong');
+  	}
 });
 
-// THIS MUST BE THIS WAY
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
